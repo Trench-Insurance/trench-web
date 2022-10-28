00f9a5623b5bdadeabde76236d8430bc3e717180
AxelDevara <axeldevara@gmail.com>
2022-10-28 11:53:09 +0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import "../app.postcss";
+</script>
+
+<div class="fixed top-[50%] left-[50%] bg-neutral-800 h-20 rounded-r-full w-10 text-white flex items-center  text-xl">➔</div>
+<div class="grid grid-cols-4 w-full h-screen">
+ <div class="col-span-2 bg-neutral-800">
+
+ </div>
+ <div class="col-span-2 bg-orange-100 ">
+  <nav class="sticky gap-6 flex items-end justify-end px-8 py-4 w-full">
+    <a href="/">Home</a>
+    <a href="/">About</a>
+    <a href="/">How to</a>
+    <a href="/">Community</a>
+  </nav>
+  <div class="flex justify-center items-center h-[100%-56px]">
+    <div class="overflow-y-scroll px-20 py-8 h-[65%]">
+    <slot />
+  </div>
+  </div>
+  
+</div>
+</div>
+
